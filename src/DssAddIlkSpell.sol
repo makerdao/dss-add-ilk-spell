539afe1f051a32f5daabaf4a1304264889746d6b
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2019-02-20 13:07:04 -0300

    Initial commit

--- src/DssAddIlkSpell.sol
+++ src/DssAddIlkSpell.sol
@@ -1,4 +1,106 @@
 pragma solidity ^0.5.4;
 
+contract SpotterLike {
+    function poke(bytes32) public;
+}
+
+contract MomLike {
+    function execute(address, bytes memory) public;
+}
+
 contract DssAddIlkSpell {
+    bool done;
+    bytes32 ilk;
+    address vat;
+    address pit;
+    address cat;
+    address jug;
+    address spotter;
+    address mom;
+    address momLib;
+    address adapter;
+    address mover;
+    address pip;
+    address flip;
+    uint line;
+    uint mat;
+    uint tax;
+    uint lump;
+    uint chop;
+
+    uint constant ONE = 10 ^ 27;
+
+    constructor(bytes32 ilk_, address[11] memory addrs, uint[5] memory values) public {
+        ilk = ilk_;
+        vat = addrs[0];
+        pit = addrs[1];
+        cat = addrs[2];
+        jug = addrs[3];
+        spotter = addrs[4];
+        mom = addrs[5];
+        momLib = addrs[6];
+        adapter = addrs[7];
+        mover = addrs[8];
+        pip = addrs[9];
+        flip = addrs[10];
+        line = values[0];
+        mat = values[1];
+        tax = values[2];
+        chop = values[3];
+        lump = values[4];
+    }
+
+    function momExecute(bytes memory data) internal {
+        MomLike(mom).execute(
+            momLib,
+            data
+        );
+    }
+
+    function cast() public {
+        require(!done, "already-deployed");
+
+        momExecute(
+            abi.encodeWithSignature("init(address,bytes32)", address(vat), bytes32(ilk))
+        );
+
+        momExecute(
+            abi.encodeWithSignature("file(address,bytes32,bytes32,uint256)", address(pit), ilk, bytes32("line"), line)
+        );
+
+        momExecute(
+            abi.encodeWithSignature("file(address,bytes32,address)", address(spotter), ilk, address(pip))
+        );
+        momExecute(
+            abi.encodeWithSignature("file(address,bytes32,bytes32,uint256)", address(spotter), ilk, bytes32("mat"), mat)
+        );
+        SpotterLike(spotter).poke(ilk);
+
+        momExecute(
+            abi.encodeWithSignature("file(address,bytes32,bytes32,address)", address(cat), ilk, bytes32("flip"), address(flip))
+        );
+        momExecute(
+            abi.encodeWithSignature("file(address,bytes32,bytes32,uint256)", address(cat), ilk, bytes32("lump"), lump)
+        );
+        momExecute(
+            abi.encodeWithSignature("file(address,bytes32,bytes32,uint256)", address(cat), ilk, bytes32("chop"), chop)
+        );
+
+        momExecute(
+            abi.encodeWithSignature("init(address,bytes32)", address(jug), bytes32(ilk))
+        );
+        momExecute(
+            abi.encodeWithSignature("file(address,bytes32,bytes32,uint256)", address(jug), ilk, bytes32("tax"), tax)
+        );
+
+        // Internal auth
+        momExecute(
+            abi.encodeWithSignature("rely(address,address)", address(vat), address(adapter))
+        );
+        momExecute(
+            abi.encodeWithSignature("rely(address,address)", address(vat), address(mover))
+        );
+
+        done = true;
+    }
 }
--- src/DssAddIlkSpell.t.sol
+++ src/DssAddIlkSpell.t.sol
@@ -2,20 +2,122 @@ pragma solidity ^0.5.4;
 
 import "ds-test/test.sol";
 
+import "dss-deploy/DssDeploy.t.base.sol";
+
 import "./DssAddIlkSpell.sol";
 
-contract DssAddIlkSpellTest is DSTest {
+import {PipLike} from "dss-deploy/poke.sol";
+
+contract DssAddIlkSpellTest is DssDeployTestBase {
     DssAddIlkSpell spell;
 
+    bytes32 constant ilk = "NCT"; // New Collateral Type
+    DSToken nct;
+    GemJoin nctJoin;
+    GemMove nctMove;
+    Flipper nctFlip;
+    DSValue nctPip;
+
     function setUp() public {
-        spell = new DssAddIlkSpell();
+        super.setUp();
+        deploy();
+
+        nct = new DSToken(ilk);
+        nct.mint(1 ether);
+        nctJoin = new GemJoin(address(vat), ilk, address(nct));
+        nctMove = new GemMove(address(vat), ilk);
+        nctPip = new DSValue();
+        nctPip.poke(bytes32(uint(300 ether)));
+        nctFlip = flipFab.newFlip(address(daiMove), address(nctMove));
+
+        spell = new DssAddIlkSpell(
+            ilk,
+            [
+                address(vat),
+                address(pit),
+                address(cat),
+                address(jug),
+                address(spotter),
+                address(mom),
+                address(momLib),
+                address(nctJoin),
+                address(nctMove),
+                address(nctPip),
+                address(nctFlip)
+            ],
+            [
+                10000 ether, // line
+                1500000000 ether, // mat
+                1.05 * 10 ** 27, // tax
+                ONE, // chop
+                10000 ether // lump
+            ]
+        );
+
+        authority.setRootUser(address(spell), true);
+
+        spell.cast();
+
+        nct.approve(address(nctJoin), 1 ether);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function testVariables() public {
+        (,uint line) = pit.ilks(ilk);
+        assertEq(line, uint(10000 ether));
+        (PipLike pip, uint mat) = spotter.ilks(ilk);
+        assertEq(address(pip), address(nctPip));
+        assertEq(mat, uint(1500000000 ether));
+        (uint tax,) = jug.ilks(ilk);
+        assertEq(tax, uint(1.05 * 10 ** 27));
+        (address flip, uint chop, uint lump) = cat.ilks(ilk);
+        assertEq(flip, address(nctFlip));
+        assertEq(chop, ONE);
+        assertEq(lump, uint(10000 ether));
+        assertEq(vat.wards(address(nctJoin)), 1);
+        assertEq(vat.wards(address(nctMove)), 1);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testFrob() public {
+        assertEq(dai.balanceOf(address(this)), 0);
+        nctJoin.join(bytes32(bytes20(address(this))), 1 ether);
+
+        pit.frob(ilk, bytes32(bytes20(address(this))), bytes32(bytes20(address(this))), bytes32(bytes20(address(this))), 1 ether, 100 ether);
+
+        daiJoin.exit(bytes32(bytes20(address(this))), address(this), 100 ether);
+        assertEq(dai.balanceOf(address(this)), 100 ether);
+    }
+
+    function testFlip() public {
+        nctJoin.join(bytes32(bytes20(address(this))), 1 ether);
+        pit.frob(ilk, bytes32(bytes20(address(this))), bytes32(bytes20(address(this))), bytes32(bytes20(address(this))), 0.5 ether, 100 ether); // Maximun DAI generated
+        nctPip.poke(bytes32(uint(300 ether - 1))); // Decrease price in 1 wei
+        spotter.poke(ilk);
+        uint nflip = cat.bite(ilk, bytes32(bytes20(address(this))));
+        assertEq(vat.gem(ilk, bytes32(bytes20(address(nctFlip)))), 0);
+        uint batchId = cat.flip(nflip, 100 ether);
+        assertEq(vat.gem(ilk, bytes32(bytes20(address(nctFlip)))), mul(0.5 ether, ONE));
+
+        address(user1).transfer(10 ether);
+        user1.doEthJoin(ethJoin, bytes32(bytes20(address(user1))), 10 ether);
+        user1.doFrob(pit, "ETH", bytes32(bytes20(address(user1))), bytes32(bytes20(address(user1))), bytes32(bytes20(address(user1))), 10 ether, 1000 ether);
+
+        address(user2).transfer(10 ether);
+        user2.doEthJoin(ethJoin, bytes32(bytes20(address(user2))), 10 ether);
+        user2.doFrob(pit, "ETH", bytes32(bytes20(address(user2))), bytes32(bytes20(address(user2))), bytes32(bytes20(address(user2))), 10 ether, 1000 ether);
+
+        user1.doHope(daiMove, address(nctFlip));
+        user2.doHope(daiMove, address(nctFlip));
+
+        user1.doTend(address(nctFlip), batchId, 0.5 ether, 50 ether);
+        user2.doTend(address(nctFlip), batchId, 0.5 ether, 70 ether);
+        user1.doTend(address(nctFlip), batchId, 0.5 ether, 90 ether);
+        user2.doTend(address(nctFlip), batchId, 0.5 ether, 100 ether);
+
+        user1.doDent(address(nctFlip), batchId, 0.4 ether, 100 ether);
+        user2.doDent(address(nctFlip), batchId, 0.35 ether, 100 ether);
+        hevm.warp(nctFlip.ttl() - 1);
+        user1.doDent(address(nctFlip), batchId, 0.3 ether, 100 ether);
+        hevm.warp(now + nctFlip.ttl() + 1);
+        user1.doDeal(address(nctFlip), batchId);
     }
 }
